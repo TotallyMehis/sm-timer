148115dd5395139a57d2cd87a8b9593ccf43bb02
TotallyMehis <totally.mehis@gmail.com>
2018-11-07 17:59:27 +0200

    Try deploying 5

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,27 @@
 echo "Deploying..."
 
 cd builds
-cd full && zip -r ../full.zip . && cd ..
-cd bhop && zip -r ../bhop.zip . && cd ..
-cd surf && zip -r ../surf.zip . && cd ..
-cd bhoplite && zip -r ../bhoplite.zip . && cd ..
+cd full && zip -rq ../full.zip . && cd ..
+cd bhop && zip -rq ../bhop.zip . && cd ..
+cd surf && zip -rq ../surf.zip . && cd ..
+cd bhoplite && zip -rq ../bhoplite.zip . && cd ..
 tar -cf influx.tar full.zip bhop.zip surf.zip bhoplite.zip
 
 echo "Uploading to site..."
 
-curl --header "Content-Type: multipart/form-data" \
-    --request POST \
-    --form "key=$INF_DEPLOY_KEY" \
-    --form "buildid=$TRAVIS_BUILD_NUMBER" \
-    --form "commithash=$TRAVIS_COMMIT" \
-    --form "commitmsg=$TRAVIS_COMMIT_MESSAGE" \
-    --form "branch=$TRAVIS_BRANCH" \
-    --form "file=@influx.tar" \
-    "$INF_DEPLOY_URL"
+CMD=curl --header "Content-Type: multipart/form-data" \
+--request POST \
+--form "key=$INF_DEPLOY_KEY" \
+--form "buildid=$TRAVIS_BUILD_NUMBER" \
+--form "commithash=$TRAVIS_COMMIT" \
+--form "commitmsg=$TRAVIS_COMMIT_MESSAGE" \
+--form "branch=$TRAVIS_BRANCH" \
+--form "file=@influx.tar" \
+"$INF_DEPLOY_URL"
+
+
+echo "Executing: $CMD"
+
+$CMD
 
 echo "Done!"
