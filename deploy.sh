641c80ccc0adaa386d4bf3e36b35356b755cea29
TotallyMehis <totally.mehis@gmail.com>
2018-11-07 18:06:41 +0200

    Try deploying 6

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,7 @@ tar -cf influx.tar full.zip bhop.zip surf.zip bhoplite.zip
 
 echo "Uploading to site..."
 
-CMD=curl --header "Content-Type: multipart/form-data" \
---request POST \
---form "key=$INF_DEPLOY_KEY" \
---form "buildid=$TRAVIS_BUILD_NUMBER" \
---form "commithash=$TRAVIS_COMMIT" \
---form "commitmsg=$TRAVIS_COMMIT_MESSAGE" \
---form "branch=$TRAVIS_BRANCH" \
---form "file=@influx.tar" \
-"$INF_DEPLOY_URL"
+CMD="curl --header \"Content-Type: multipart/form-data\" --request POST --form \"key=$INF_DEPLOY_KEY\" --form \"buildid=$TRAVIS_BUILD_NUMBER\" --form \"commithash=$TRAVIS_COMMIT\" --form \"commitmsg=$TRAVIS_COMMIT_MESSAGE\" --form \"branch=$TRAVIS_BRANCH\" --form \"file=@influx.tar\" \"$INF_DEPLOY_URL\""
 
 
 echo "Executing: $CMD"
