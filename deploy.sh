38fb74f09f5220d3d6a03ea4aef9b9ae50195ed7
TotallyMehis <totally.mehis@gmail.com>
2018-11-07 19:42:24 +0200

    Testing deployment

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,19 @@
 
 echo "Deploying..."
 
+# Zip em and roll em into a tar ball
 cd builds
-cd full && zip -rq ../full.zip . && cd ..
-cd bhop && zip -rq ../bhop.zip . && cd ..
-cd surf && zip -rq ../surf.zip . && cd ..
-cd bhoplite && zip -rq ../bhoplite.zip . && cd ..
+cd full && zip -rq9 ../full.zip . && cd ..
+cd bhop && zip -rq9 ../bhop.zip . && cd ..
+cd surf && zip -rq9 ../surf.zip . && cd ..
+cd bhoplite && zip -rq9 ../bhoplite.zip . && cd ..
 tar -cf influx.tar full.zip bhop.zip surf.zip bhoplite.zip
 
 echo "Uploading to site..."
 
 curl -H "Content-Type: multipart/form-data" -X POST \
 -F "key=$INF_DEPLOY_KEY" \
--F "buildid=$TRAVIS_BUILD_NUMBER" \
+-F "buildnum=$TRAVIS_BUILD_NUMBER" \
 -F "commithash=$TRAVIS_COMMIT" \
 -F "commitmsg=$TRAVIS_COMMIT_MESSAGE" \
 -F "branch=$TRAVIS_BRANCH" \
